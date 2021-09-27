fc4c79796b1d7993ddf3b370bb3295ac1c5c3e82
xuanthanh1204 <xuanthanh120420@gmail.com>
2021-09-27 12:34:56 +0700

    add

--- src/test/java/com/example/spring_boot_jwt_NguyenXuanThanh/SpringBootJwtNguyenXuanThanhApplicationTests.java
+++ src/test/java/com/example/spring_boot_jwt_NguyenXuanThanh/SpringBootJwtNguyenXuanThanhApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootJwtPhamNgocNhuYApplicationTests {
+class SpringBootJwtNguyenXuanThanhApplicationTests {
 
     @Test
     void contextLoads() {
